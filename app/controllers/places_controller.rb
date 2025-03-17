e1476c64abad5ea151b25eb3a627a4e70e4a8253
kelsey Brown <kelsey.brown@kellogg.northwestern.edu>
2025-03-17 22:34:32 +0000

    HW 4 initial commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,8 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+    @place = Place.new
+    puts @places.inspect
   end
 
   def show
@@ -13,10 +15,46 @@ class PlacesController < ApplicationController
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @place = Place.new
+      @post["user_id"] = @current_user["id"] 
+      @place["name"] = params["name"]
+      @place.save
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places"
   end
 
+  def edit
+    # find a Place
+    @place = Place.find_by({ "id" => params["id"] })
+    # render view with edit Place form
+  end
+
+  def update
+    # find a Place
+    @Place = Place.find_by({ "id" => params["id"] })
+
+    # assign user-entered form data to Company's columns
+    @place["name"] = params["name"]
+
+    # save Company row
+    @place.save
+
+    # redirect user
+    redirect_to "/places"
+  end
+
+  def destroy
+    # find a Place
+    @place = Place.find_by({ "id" => params["id"] })
+
+    # destroy Company row
+    @place.destroy
+
+    # redirect user
+    redirect_to "/places"
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"]})
+    if @user != nil
+        if BCrypt::Password.new(@user["password"]) == params["password"]
+          session["user_id"] = @user["id"]
+          puts session
+          # flash["notice"] = "Welcome, #{@user["username"]}!"
+          redirect_to "/places"
+        else
+          flash["notice"] = "Try again."
+          redirect_to "/login"
+        end 
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+        flash["notice"] = "Ending Session."
+    redirect_to "/login"
   end
 end
   
